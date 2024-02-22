ad45dc80e39b40fa4d0bdf523b9772c17473b191
christine <christinec.dev@gmail.com>
2024-02-22 14:18:30 +0200

    Add files via upload

---
+++ Part_12/Scripts/Obstacles.gd
@@ -0,0 +1,24 @@
+### Obstacles.gd
+
+extends Area3D
+
+func _ready():
+	spawn_obstacle()
+	
+# Spawn obstacle	
+func spawn_obstacle():
+	var obstacle_resource = null
+	obstacle_resource = Global.obstacle_resources[randi() % Global.obstacle_resources.size()]
+	var obstacle_instance = obstacle_resource.instantiate()
+	add_child(obstacle_instance)
+	
+# Player and Obstacle collision
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		if Global.lives > 0:
+			Global.lives -= 1
+			Global.lives_updated.emit()
+			print("Deducting Lives")
+		else:
+			print("Game over")
+		
